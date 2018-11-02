7733e4f3d6b34094fcd28f3728d38d49ef1e4e51
Ryosuke <>
2018-11-02 20:51:58 +0900

    webpack-dev-serverの追加

--- src/js/renderer/index.tsx
+++ src/js/renderer/index.tsx
@@ -3,12 +3,14 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 // Routingの定義
 const appRouting = (
   <Router>
     <Layout>
-      <Route path="/" component={Home} />
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
     </Layout>
   </Router>
 );
--- src/js/renderer/pages/About.tsx
+++ src/js/renderer/pages/About.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
-export default class Home extends React.Component {
+export default class About extends React.Component {
   render() {
     return (
       <div>
-        HOME
+        ABOUT
       </div>
     );
   }
