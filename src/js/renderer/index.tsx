008fc074a958005213e02cdd6a0dc27897e05359
Ryosuke <>
2018-11-05 21:22:49 +0900

    ブラウザでHMR動作済み

--- src/js/renderer/index.tsx
+++ src/js/renderer/index.tsx
@@ -4,16 +4,23 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { AppContainer } from 'react-hot-loader'
 
 // Routingの定義
 const appRouting = (
-  <Router>
-    <Layout>
-      <Route exact={true} path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Layout>
-  </Router>
+  <AppContainer>
+    <Router>
+      <Layout>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Layout>
+    </Router>
+  </AppContainer>
 );
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // Applicationをrendering
 render(appRouting, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const path = require('path');
 
 const baseConfig =  {
   mode: 'development',
@@ -6,24 +7,45 @@ const baseConfig =  {
     __dirname: false
   },
   output: {
-    publicPath: 'http://localhost:8088/dist',
-    filename: '[name].js'
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/')
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
-        use: [
-          'ts-loader'
-        ]
-      }
-    ]
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+              'react-hot-loader/babel',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    port: 8088,
+    contentBase: 'dist'
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
@@ -32,7 +54,7 @@ const baseConfig =  {
 
 const mainConfig = Object.assign({}, baseConfig, {
   name: 'main',
-  target: 'electron-main',
+  // target: 'electron-main',
   entry: {
     "js/main/index": "./src/js/main/index.ts"
   }
@@ -40,10 +62,10 @@ const mainConfig = Object.assign({}, baseConfig, {
 
 const rendererConfig = Object.assign({}, baseConfig, {
   name: 'renderer',
-  target: 'electron-renderer',
+  // target: 'electron-renderer',
   entry: {
     "js/renderer/index": "./src/js/renderer/index.tsx"
   }
 });
 
-module.exports = [mainConfig, rendererConfig];
+module.exports = [rendererConfig];
